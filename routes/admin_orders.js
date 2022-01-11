60743ae3d779ce69923fe3271d9c9c2a2c56b090
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-01-11 16:04:03 -0500

    change js and ejs files

--- routes/admin_orders.js
+++ routes/admin_orders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
+// Show customer orders to the browser
 module.exports = (db) => {
   router.get("/",(req, res) => {
       db.query(`
@@ -15,6 +16,7 @@ module.exports = (db) => {
       })
   });
 
+  // Recieve form data from item_new.ejs
   // router.post("/", (req, res) => {
   //   const name = ;
   //   const price = ;
@@ -23,13 +25,22 @@ module.exports = (db) => {
   //   res.redirect("/food_items");
   // });
 
+  // Show "create new item form" page to the browser
+  router.get("/items/new", (req, res) => {
+    const templateVars = {
+      user: users[req.session["user_id"]],
+    };
+    if (!req.session.user_id) {
+      res.redirect("/login");
+    } else {
+      res.render("item_new", templateVars);
+    }
+  });
+
   return router;
 };
 
-// router.get("/admin", (req, res) => {
 
-//     res.render("item_new", templateVars);
-// });
 
 
 
