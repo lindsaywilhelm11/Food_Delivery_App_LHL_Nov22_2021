084a57fee3315d54e9ce379d538c0c2b2b3ed7d1
satya sindhu <movva001@gmail.com>
2022-01-11 20:15:29 -0500

    added customer order route to server.js, added ejs file, added route code in customer.js file

--- routes/customer_orders/customer.js
+++ routes/customer_orders/customer.js
@@ -3,34 +3,9 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-
-
-
-  router.post("/login", (req, res) => {
-    const {email, password} = req.body;
-    // if (!email || !password) {
-    //   res.send('Not authorized');
-    //   return
-    // }
-    dbFuncs.login(db, email, password)
-    .then(user => {
-      if(!user) {
-        res.statusCode = 401;
-        res.send({error: 'Not authorized'});
-        return;
-      }
-      req.session.userId = user.id;
-      const restaurant_name = "Five Guys";
-      res.redirect(`/restaurants/${restaurant_name}/portal`);
-    })
-    .catch(err=> {res.send(err.message)});
-  });
-
-  return router;
-};  router.get("/", (req, res) => {
+  router.get("/", (req, res) => {
     const customer_orders = req.session.customer_orders|| {};
     const id1 = Object.keys(customer_orders);
-    const customer_orders = [];
     console.log(id1)
     if (id1.length === 0) {
       return res.render('customer_orders', {
@@ -49,21 +24,18 @@ module.exports = (db) => {
             Object.assign({}, item, { quantity })
           );
         }
-        res.render('customer_order', {
+        res.render('customer_orders', {
           customer_orders,
         });
       })
       .catch(e => {
         console.log(e);
-        res.render('customer_order', {
+        res.render('customer_orders', {
           customer_orders,
           "error" : e,
         })
       })
   });
-
-
-
   router.post("/", (req, res) => {
     let customer_orders = {};
     if (req.session.cart) {
@@ -76,21 +48,8 @@ module.exports = (db) => {
     req.session.customer_orders = customer_orders;
     res.redirect('food_items');
   });
-  return router;
-}
-
-
-
-router.post("/edit", function (req, res) => {
-
-
-
-
-}
-
-
-
-
+  router.post("/edit", (req, res) => {
+  });
 
   router.post("/delete", (req, res) => {
     if (users[req.session.id1]) {
@@ -109,5 +68,6 @@ router.post("/edit", function (req, res) => {
     }
   })
 
+  return router;
+};
 
-module.exports = router;
